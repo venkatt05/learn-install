e087fc385313728cf713db314becaf40d5c97166
sthoomati <sasikala.thoomati@anthology.com>
2024-04-17 10:40:05 +0530

    Added execute permission for pre.zsh

--- v2/pre.zsh
+++ v2/pre.zsh
@@ -25,14 +25,15 @@ if [  ! -f ~/.ssh/id_ed25519 ]; then
   # Add SSH key to agent
   ssh-add --apple-use-keychain $HOME/.ssh/id_ed25519 &&
 
-  echo -e "\e[33mYour public SSH key Highlighted in Green Colour and is already copied:\e[0m" &&
-  echo -e "\e[32m$(cat ~/.ssh/id_ed25519.pub)\e[0m"
-  echo "\n"
-  echo -e "\e[33m*******************  Read the Below Notes Carefully*********************\e[0m
-    Add the above Public SSH to your GitHub account and configure it.
-    You can see there is a GiHub URL opened in your default browser. Add the SSH key to your GitHub account and then once the key is added, click on \e[1;32mconfigure sso\e[0m button and authorize all the projects.
-    \n
-    For more details Refer: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account. Proceed with the next steps once done."
+  echo "\a \a \a \a"
+  echo -e "\e[33m*******************  Read the Below Notes Carefully*********************\e[0m"
+  echo -e "\n \e[33mYour public SSH key Highlighted in Green Colour and is already copied:\e[0m" &&
+  echo -e "\n \e[32m$(cat ~/.ssh/id_ed25519.pub)\e[0m"
+  echo "\n 1. You can see there is a GiHub URL opened in your default browser."
+  echo "\n 2. press "command + v" to paste the SSH into the value box."
+  echo "\n 3. click on \e[1;32mconfigure sso\e[0m button and authorize all the projects."
+  echo -e "\n"
+  echo -e "For more details Refer: https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account. Proceed with the next steps once done."
 
   pbcopy < ~/.ssh/id_ed25519.pub
   open "https://github.com/settings/ssh/new"
