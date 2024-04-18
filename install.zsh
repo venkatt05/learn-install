2e37d4e41bd44285668cd920f6c5235af93e2f8b
sthoomati <sasikala.thoomati@anthology.com>
2024-04-18 15:33:22 +0530

    Enable ultra experience

--- install.zsh
+++ install.zsh
@@ -2,6 +2,12 @@
 password="$1"
 echo $password |  sudo -v
 
+print_congrats() {
+echo -e "\e[1m \e[32m ********* Congratulations!!! ********  \e[0m" 
+echo -e "\e[1m \e[36m Your Learn Application is Up !!! and opened in your default browser\e[0m"  
+echo -e "\e[36m  Userid/password: administrator/changeme .  \e[0m"                                  
+}
+
 error() {
   local input_message="$1"
   local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
@@ -18,6 +24,11 @@ echo $password |  sudo -v
     gdl installLearn &&
     echo -e "\e[33m Starting Learn Application... \e[0m"
     gdl startLearn
+    open "https://mylearn.int.bbpd.io"
+    print_congrats
+    
+  else
+    echo -e "\e[33m There is no Learn Folder ... \e[0m"
   fi
 }
 
@@ -38,6 +49,12 @@ install_ultra() {
     echo -e "\e[33m Starting Ultra Application... \e[0m"
     yarn start
   fi
+  $BBHOME/tools/admin/UpdateUltraUIDecision.sh --on
+
+  echo "\e[33m ********** To see the Ultra front end ********** \e[0m "
+  echo "\n 1. Go to the System Admin panel and enable Ultra by clicking on the \e[1;32m Enable! \e[0m button under -- > The Ultra experience is here! ."
+  echo "\n 2. You will be redirected--from then on--to the Ultra UI at https://mylearn.int.bbpd.io/ultra."
+
 }
 
 install_Learn || error "Failed to install Learn"
@@ -48,3 +65,4 @@ install_ultra || { error "Error: Failed to install Ultra."; exit 1; }
 else
   error "Error: Failed to start"
 fi
+
