c5721035336d82b3c60c312be07c77b09c1aa785
sthoomati <sasikala.thoomati@anthology.com>
2024-07-09 16:44:47 +0530

    Added changes in Install-ultra

--- install.zsh
+++ install.zsh
@@ -96,13 +96,16 @@ install_ultra() {
     cd $HOME/work &&
     cd $HOME/work/ultra/apps/ultra-ui &&
     echo -e "\e[33m Starting Ultra Application... \e[0m"
-    yarn start
+    yarn start >> ~/install.log 2>&1 &
+    echo -e "\e[33m Started Ultra Application Successfully... \e[0m"
+    $bbHOME/tools/admin/UpdateUltraUIDecision.sh --on
+    echo "\e[33m ********** To see the Ultra front end ********** \e[0m "
+    echo "\n 1. Go to the System Admin panel and enable Ultra by clicking on the \e[1;32m Enable! \e[0m button under -- > The Ultra experience is here! ."
+    echo "\n 2. You will be redirected--from then on--to the Ultra UI at https://mylearn.int.bbpd.io/ultra."
   fi
-  $bbHOME/tools/admin/UpdateUltraUIDecision.sh --on
+ 
 
-  echo "\e[33m ********** To see the Ultra front end ********** \e[0m "
-  echo "\n 1. Go to the System Admin panel and enable Ultra by clicking on the \e[1;32m Enable! \e[0m button under -- > The Ultra experience is here! ."
-  echo "\n 2. You will be redirected--from then on--to the Ultra UI at https://mylearn.int.bbpd.io/ultra."
+  
 
 }
 
