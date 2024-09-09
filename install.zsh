d2cb00168fd36c0d1de3122bc92fafd350eb7942
sthoomati <sasikala.thoomati@anthology.com>
2024-09-09 15:46:52 +0530

    Added to follow steps in the document

--- install.zsh
+++ install.zsh
@@ -34,10 +34,7 @@ print_congrats() {
 echo -e "\e[1m \e[32m **********************    Congratulations!!!   ***********************  \e[0m" 
 open "https://mylearn.int.bbpd.io"
 echo -e "\e[1m \e[36m Your Learn Application is Up !!! and opened in your default browser\e[0m"  
-echo -e "\e[36m  Userid/password: administrator/changeme .  \e[0m"  
-echo "\e[33m ********** To see the Ultra front end ********** \e[0m "
-echo "\n 1. Go to the System Admin panel and enable Ultra by clicking on the \e[1;32m Enable! \e[0m button under -- > The Ultra experience is here! ."
-echo "\n 2. You will be redirected--from then on--to the Ultra UI at https://mylearn.int.bbpd.io/ultra."                                
+echo -e "\e[33m Go Back to the document and follow Step 7 \e[0m"                             
 }
 
 error() {
